7eb6cfe59ea7696979ca45e3b4c7fdff40d4f7d3
PRM710 <99472825+PRM710@users.noreply.github.com>
2025-01-26 18:50:06 +0530

    Update Chicken.jsx

--- src/components/Chicken.jsx
+++ src/components/Chicken.jsx
@@ -5,39 +5,39 @@ import '../styles/Chicken.css';
 
 function Chicken() {
   const handleOrderClick = () => {
+    // Instagram username and links
+    const username = 'its_d_kitchen';
+    const dmLink = `https://www.instagram.com/direct/t/17847224382319784/`;
+    const appDeepLink = `instagram://user?username=${username}`;
 
-    const dmLink = 'https://www.instagram.com/direct/t/17847224382319784/';
-    const appDeepLink = 'instagram://direct/t/17847224382319784';
-
-
+    // Check if user is on a mobile device
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
- 
     if (isMobile) {
-      window.location.href = appDeepLink; 
+      window.location.href = appDeepLink; // Open Instagram app to the user's profile
     } else {
-      window.location.href = dmLink; 
+      window.location.href = dmLink; // Open Instagram DM in browser
     }
   };
 
   return (
-    <div className='food2'>
+    <div className="food2">
       <Logo />
-      <div className='food2--column'>
-        <div className='left--side2'>
-          <div className='food2--text'>
-            <h2 className='food2--title'> Chicken </h2>
+      <div className="food2--column">
+        <div className="left--side2">
+          <div className="food2--text">
+            <h2 className="food2--title"> Chicken </h2>
             <p>
-              A fusion of different traditional recipes with my own unique flair, delivering a mouthwatering and an aromatic feast.
+              A fusion of different traditional recipes with my own unique flair, delivering a mouthwatering and aromatic feast.
             </p>
           </div>
         </div>
-        <div className='right--side2'>
-          <img src={chickenimg} alt="Chicken" className='food2--img' />
+        <div className="right--side2">
+          <img src={chickenimg} alt="Chicken" className="food2--img" />
         </div>
       </div>
-      <div className='order--food2'>
-        <button className='order--button2' onClick={handleOrderClick}>
+      <div className="order--food2">
+        <button className="order--button2" onClick={handleOrderClick}>
           Order Now
         </button>
       </div>
